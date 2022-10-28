d87f1d06542d7d09588cda36abb90712017446a9
DDashi <kek-lol-2003@inbox.ru>
2022-10-28 16:04:32 +0500

    getAnswerByPage: add this function *function are changed the page if the user goes to another one *return the response depenfing on the page on it is located

--- Tinder.cs
+++ Tinder.cs
@@ -9,16 +9,32 @@ namespace telegram_bot
 {
     public class Tinder
     {
-        MainPage _mainPage;
+        public MainPage _mainPage;// TODO: добавить остальные страницы сюда
 
         public Tinder()
         {
             _mainPage = new MainPage();
+            // TODO: добавить страницы в конструктор
         }
 
-        public Message getAnswer(BotUser user, Message message)
+        public Message getAnswerByPage(BotUser user, Message message)
         {
-            return null;
+            // если пользователь перемещается между страницами
+            if (message in dbPages) // TODO: добавить бд 
+            {
+                // TODO: изменить статус пользователя (т.е. его страницу)
+            }
+
+            switch (user.status) // TODO: добавить поля в user
+            {
+                case "/main":
+                    return _mainPage.getAnswer();
+                case "/edit_profile":
+                    return null;
+                default:
+                    return null;
+
+            }
         }
 
 
