7557138e459d327d5a5fc874c295a13fa0f2c432
ilya <da.tot.samy.ilya@gmail.com>
2022-11-29 22:30:10 +0500

    Db: removed KeyWords methods

--- data_base/Db.cs
+++ data_base/Db.cs
@@ -1,6 +1,6 @@
 ﻿using Newtonsoft.Json;
-using Telegram.Bot.Types;
-using telegram_bot.Tinder.enums;
+using telegram_bot.bot;
+using File = System.IO.File;
 
 namespace telegram_bot.data_base
 {
@@ -11,45 +11,53 @@ namespace telegram_bot.data_base
 
         protected Db(string dbName, string fileExtention)
         {
-            Directory.CreateDirectory(@"..\..\..\db");
-            _path = Path.Join(@"..\..\..\db", dbName + "." + fileExtention);
+            Directory.CreateDirectory(@"..\..\..\data_base\db");
+            _path = Path.Join(@"..\..\..\data_base\db", dbName + "." + fileExtention);
             Table = new Dictionary<TKey, TValue>();
         }
+
         public void Update(TKey id, TValue user)
         {
             if (!FindByKey(id))
             {
                 return;
             }
+
             Table = ReadAllTable();
             Table[id] = user;
             WriteAllTable(Table);
         }
+
         public void Add(TKey id, TValue user)
         {
             if (FindByKey(id))
             {
                 return;
             }
+
             Table = ReadAllTable();
             Table.Add(id, user);
             WriteAllTable(Table);
         }
+
         public void Delete(TKey id)
         {
             if (!FindByKey(id))
             {
                 return;
             }
+
             Table = ReadAllTable();
             Table.Remove(id);
             WriteAllTable(Table);
         }
+
         public bool FindByKey(TKey id)
         {
             Table = ReadAllTable();
             return Table.ContainsKey(id);
         }
+
         public Dictionary<TKey, TValue> ReadAllTable()
         {
             var emptyDict = new Dictionary<TKey, TValue>();
@@ -59,12 +67,14 @@ namespace telegram_bot.data_base
                 WriteAllTable(emptyDict);
                 return emptyDict;
             }
+
             var json = File.ReadAllText(_path);
             if (json == "")
             {
                 json = "{}";
                 WriteAllTable(emptyDict);
             }
+
             return JsonConvert.DeserializeObject<Dictionary<TKey, TValue>>(json);
         }
 
@@ -74,12 +84,17 @@ namespace telegram_bot.data_base
             {
                 File.Create(_path);
             }
+
             File.WriteAllText(_path, JsonConvert.SerializeObject(dict));
         }
     }
+
     public class DbUsers : Db<long, BotUser>
     {
-        public DbUsers(string dbName, string fileExtention) : base(dbName, fileExtention) { }
+        public DbUsers(string dbName, string fileExtention) : base(dbName, fileExtention)
+        {
+        }
+
         public BotUser GetOrCreate(long id, BotUser user)
         {
             if (FindByKey(id))
@@ -88,33 +103,21 @@ namespace telegram_bot.data_base
             }
             else
             {
-                var emptyGame = new Game(0, 0, 0, (int)id);
-                var newUser = new BotUser(GameStatus.NotPlaying, id, user.Name, emptyGame);
+                var newUser = new BotUser(id, user.firstName, user.photoId);
                 Add(id, newUser);
                 return user;
             }
         }
+
         public BotUser GetByKey(long id)
         {
             if (!FindByKey(id))
             {
                 return null;
             }
+
             Table = ReadAllTable();
             return Table[id];
-        }
-    }
-    public class DbKeyWords : Db<string, string>
-    {
-        public DbKeyWords(string dbName, string fileExtention) : base(dbName, fileExtention) { }
-        public string GetByKey(string key)
-        {
-            Table = ReadAllTable();
-            if (!FindByKey(key))
-            {
-                return null;
-            }
-            return Table[key];
         }
     }
 }
