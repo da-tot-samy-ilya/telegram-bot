4c18b4a723079d70eea527972f3a31753022887f
ilya <da.tot.samy.ilya@gmail.com>
2022-12-18 12:05:08 +0500

    Added file Db

--- data_base/UsersDb.cs
+++ data_base/UsersDb.cs
@@ -1,55 +1,36 @@
-﻿using MongoDB.Bson;
-using MongoDB.Driver;
-using telegram_bot.bot;
+﻿using telegram_bot.bot;
 
-namespace telegram_bot
+namespace telegram_bot.data_base
 {
-    public class UsersDb
+    public class UsersDb : Db<long, BotUser>
     {
-        private readonly MongoClientSettings _settings;
-        private MongoClient _client;
-        private IMongoDatabase _db;
-        private IMongoCollection<BotUser> _collection;
-    
-        public UsersDb(string dbName, string collectionName)
+        public UsersDb(string dbName, string fileExtention) : base(dbName, fileExtention)
         {
-            _settings = MongoClientSettings.FromConnectionString(
-                "mongodb+srv://aboba:1234@tinder.sltinsc.mongodb.net/?retryWrites=true&w=majority");
-            _settings.ServerApi = new ServerApi(ServerApiVersion.V1);
-            _client = new MongoClient(_settings);
-            _db = _client.GetDatabase(dbName); //"tinder-users"
-            _collection = _db.GetCollection<BotUser>(collectionName); //"users"
         }
-        public void Update(long userid, BotUser user)
+
+        public BotUser GetOrCreate(long id, string userName, int lastMessageId)
         {
-            var filter = Builders<BotUser>.Filter.Eq(p => p.id, user.id);
-            _collection.ReplaceOne(filter, user);
-        }
-        public void Add(BotUser user)
-        {
-            if (_collection.CountDocuments(p => p.id == user.id) == 0)
+            if (FindByKey(id))
             {
-                _collection.InsertOne(user);
+                return GetByKey(id);
+            }
+            else
+            {
+                var newUser = new BotUser(id, userName, lastMessageId);
+                Add(id, newUser);
+                return newUser;
             }
         }
-        public void Delete(BotUser user)
+
+        public BotUser GetByKey(long id)
         {
-            _collection.DeleteMany(p => p.id == user.id);
-        }
-        public List<BotUser> ReadAll()
-        {
-            return _collection.Find(new BsonDocument()).ToList();
-        }
-        public BotUser GetOrCreate(long userId, String userName, int lastMessageId)
-        {
-            List<BotUser> users = _collection.Find(p=>p.id == userId).ToList();
-            if (users.Count == 0)
+            if (!FindByKey(id))
             {
-                var user = new BotUser(userId, userName, lastMessageId);
-                Add(user);
-                return user;
+                return null;
             }
-            return users[0];
+
+            Table = ReadAllTable();
+            return Table[id];
         }
     }
-}
+};
